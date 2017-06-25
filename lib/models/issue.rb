103e4c79f1c0be74a08dfda122a4d9108bdf0ce3
Junaid Farooq <junaid@evercam.io>
2017-06-25 19:40:16 +0500

    Added status to issues.

--- lib/models/issue.rb
+++ lib/models/issue.rb
@@ -1,7 +1,9 @@
 class Issue
+  STATUSES = %i(new open needs_info cannot_duplicates closed)
   include Mongoid::Document
   include Mongoid::Timestamps::Updated
 
   field :name, type: String
   field :description, type: String
+  field :status, type: Symbol, default: :new
 end
