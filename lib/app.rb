6f072585521f7e001c5bcb92ba9388e11d55c337
Junaid Farooq <junaid@evercam.io>
2017-06-25 16:55:19 +0500

    Create and Enlist new issues.

--- lib/app.rb
+++ lib/app.rb
@@ -1,12 +1,28 @@
 require_relative "models/issue"
 
 class App < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
   get "/" do
-    "hello"
+    redirect "/issues"
   end
 
   get "/issues" do
     @issues = Issue.all
-    @issues
+
+    haml :"issues/index"
+  end
+
+  get "/issues/new" do
+    @issue = Issue.new
+    haml :"issues/new"
+  end
+
+  post "/issues" do
+    @issue = Issue.new params[:issue]
+    if @issue.save
+      redirect "/"
+    end
   end
 end
